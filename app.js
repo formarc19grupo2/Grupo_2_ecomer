dff5a44e77b33de761dfdb2fe26ab42888b006f9
Lucasfmonsores <lucasfmonsores@gmail.com>
2022-12-26 20:07:31 -0300

    correccion archivo app.js con rutas

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ app.use(express.static('public'));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./view/index.html"));
 });
-app.get("/babbage", (req, res) => {
+app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "./view/login.html"));
 });
-app.get("/berners-lee", (req, res) => {
+app.get("/productCart", (req, res) => {
   res.sendFile(path.join(__dirname, "./view/productCart.html"));
 });
-app.get("/clarke", (req, res) => {
+app.get("/productDetail", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/productDetail.html"));
 });
-app.get("/hamilton", (req, res) => {
+app.get("/register", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/register.html"));
 });
 
@@ -25,3 +25,4 @@ app.get("/hamilton", (req, res) => {
 app.listen(3030, () => {
   console.log("server online on localhost:3030");
 });
+
