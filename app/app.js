002a1b7d7b0753da599a483820632a930b0fca8e
OscarChacoma <ochacoma2.0@gmail.com>
2023-01-03 14:22:47 -0300

    Agrego estructura y estilos de footer

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ const path = require('path')
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./view/index.html"));
+  res.sendFile(path.join(__dirname, "./view/home.html"));
 });
 app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "./view/login.html"));
