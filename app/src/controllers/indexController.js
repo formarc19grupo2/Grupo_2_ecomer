292370604cebd15e29f26ea541e3a7825958619e
OscarChacoma <ochacoma2.0@gmail.com>
2023-02-04 01:10:15 -0300

    Agregamos formularios de creacion y edicion de productos

--- app/src/controllers/indexController.js
+++ app/src/controllers/indexController.js
@@ -2,25 +2,29 @@ const path = require("path")
 
 module.exports = {
 
-
-
     index: (req, res) => {
         return res.render("home")
     },
 
     detail: (req, res) => {
-        return res.render("productDetail")
+        return res.render("products/productDetail")
     },
 
     cart: (req, res) => {
-        return res.render("productCart")
+        return res.render("products/productCart")
     },
 
     login: (req, res) => {
-        return res.render("login")
+        return res.render("users/login")
     },
 
     register: (req, res) => {
-        return res.render("register")
+        return res.render("users/register")
+    },
+    create: (req, res) => {
+        return res.render("products/create")
+    },
+    edit: (req, res) => {
+        return res.render("products/edit")
     }
 }
--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -7,5 +7,6 @@ routes.get("/productDetail",controller.detail);
 routes.get("/productCart", controller.cart);
 routes.get("/login", controller.login);
 routes.get("/register", controller.register);
-
+routes.get("/create", controller.create);
+routes.get("/edit", controller.edit);
 module.exports = routes;
