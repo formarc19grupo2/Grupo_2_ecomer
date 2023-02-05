ed83471b853d7f16ae1bd280d71290db659db46e
OscarChacoma <ochacoma2.0@gmail.com>
2023-02-05 02:08:25 -0300

    Agrego actualizaciones a vistas detalle de producto y formularios

--- app/src/controllers/indexController.js
+++ app/src/controllers/indexController.js
@@ -1,5 +1,11 @@
-const path = require("path")
-const products = require("../database/productsDataBase.json")
+const path = require("path");
+const fs = require('fs');
+
+const productsFilePath = path.join(__dirname,"../database/productsDataBase.json");
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const writeJson = (products) => {
+	fs.writeFileSync(productsFilePath, JSON.stringify(products), {encoding: "utf-8"})
+}
 
 
 module.exports = {
@@ -11,7 +17,12 @@ module.exports = {
     },
 
     detail: (req, res) => {
-        return res.render("products/productDetail")
+        let productId = req.params.id;
+        let product = products.find(product => product.id == productId);
+
+        res.render("products/productDetail", {
+            product
+        })
     },
 
     cart: (req, res) => {
@@ -27,6 +38,25 @@ module.exports = {
     },
     create: (req, res) => {
         return res.render("products/create")
+    },
+    store: (req, res) =>{
+        let lastId = products[products.length -1].id;
+		
+
+		let newProduct = {
+			id: lastId + 1,
+			name: req.body.name,
+			price: req.body.price,
+			discount: req.body.discount,
+			category: req.body.category,
+			description: req.body.description,
+			image: "default-image.png",
+		}
+		products.push(newProduct);
+
+		writeJson(products); //escribe el JSON - y persiste 
+		
+		res.redirect("/home/");
     },
     edit: (req, res) => {
         return res.render("products/edit")
--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -3,10 +3,11 @@ const routes = express.Router();
 const controller = require("../controllers/indexController");
 
 routes.get("/", controller.index);
-routes.get("/productDetail",controller.detail);
+routes.get("/productDetail/:id",controller.detail);
 routes.get("/productCart", controller.cart);
 routes.get("/login", controller.login);
 routes.get("/register", controller.register);
 routes.get("/create", controller.create);
+routes.post('/', controller.store); 
 routes.get("/edit", controller.edit);
 module.exports = routes;
