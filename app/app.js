df5e0cb4b549a6b4116a93a26336dbc29a936eed
Lucas Monsores <lucasfmonsores@gmail.com>
2023-02-26 12:23:51 -0300

    checkpoint

--- app/app.js
+++ app/app.js
@@ -12,14 +12,14 @@ const path = require('path');
 
 app.use(express.static('public'));
 app.set ("view engine", "ejs");
-
+app.set("views", "./src/views");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(cookieParser());
 
-app.set('views', path.join(__dirname, './src/views'));
+
 app.use(methodOverride('_method'));
 
 /* Routers */
