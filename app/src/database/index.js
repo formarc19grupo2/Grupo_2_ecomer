e61654e74b1041037a1fedb56c918fcd94309d30
OscarChacoma <ochacoma2.0@gmail.com>
2023-03-21 13:53:12 -0300

    correccion crud

--- app/src/database/index.js
+++ app/src/database/index.js
@@ -2,12 +2,12 @@ const path = require("path");
 const fs = require("fs");
 
 module.exports = {
-    /* readJSON: (json) => {
+    readJSON: (json) => {
         return JSON.parse(fs.readFileSync(path.resolve(__dirname, json)))
     },
     writeJson: (json, array) => {
         fs.writeFileSync(path.resolve(__dirname, json), JSON.stringify(array,null,3), 'utf-8')
-    }, */
+    },
 
     users:JSON.parse(fs.readFileSync(path.join(__dirname, "/users.json"), "utf-8")),
     writeUsersJson: (data) => {
