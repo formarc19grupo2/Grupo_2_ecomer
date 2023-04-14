c61110f1d03524601645a404610f5cc14ba0a63b
germanlasserre <germanlasserre@gmail.com>
2023-04-14 19:02:48 -0300

    actualizo nombre de database en config.js

--- app/src/database/config/config.js
+++ app/src/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": null,
-    "database": "db_ecommer",
+    "database": "ecommer",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
