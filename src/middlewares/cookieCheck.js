deb332f416f57e9020ecff827b542cef2c12cd56
OscarChacoma <oschacoma@gmail.com>
2023-04-19 23:22:46 -0300

    cambios en el proyecto

--- src/middlewares/cookieCheck.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res, next) => {
-    if(req.cookies.userEcomer && !req.session.user) {
-        req.session.user = req.cookies.userEcomer;
-        res.locals.user = req.session.user;
-    }
-    next();
-}
