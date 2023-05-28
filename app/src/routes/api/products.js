05f038f42d573f97507007c5cc2d64201b98b99e
OscarChacoma <oschacoma@gmail.com>
2023-05-27 23:01:16 -0300

    Agrego correccion api de productos, y vista que muestre imagen del producto correspondiente

--- app/src/routes/api/products.js
+++ app/src/routes/api/products.js
@@ -1,10 +1,10 @@
 const express = require ("express");
 const router = express.Router();
-const {getAll,getOne,store} = require("../../controllers/api/productsApi")
+const {getAll,getOne,productsImage} = require("../../controllers/api/productsApi")
 
 router
     .get("/", getAll)
     .get("/:id", getOne)
     // .post("/api/products",store)
-
+    .get("/:id/images", productsImage)
 module.exports = router;
